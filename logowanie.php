195b391bb3c178bad4e1d09752886bab87d7b563
matsyk95 <matsyk998@gmail.com>
2016-12-12 12:36:39 +0100

    Create logowanie.php

---
+++ logowanie.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+	if (empty($_POST['username']) || empty($_POST['password'])) {
+		$error = "Haslo jest bledne";
+	}
+	else
+	{
+		// Define $username and $password
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+		
+		$username = stripslashes($username);
+		$password = stripslashes($password);
+		$validuser = "student";
+		$validpass = "zet";
+		
+		if ($username === $validuser && $password === $validpass) {
+			$_SESSION['login_user']=$username; // Initializing Session
+			header("location: profile.php"); // Redirecting To Other Page
+		} else {
+			$error = "Hasło jest błedne ";
+		}
+		
+	}
+}
+?>
