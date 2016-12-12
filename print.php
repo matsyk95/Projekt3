ce93400bfdf093adb8a79c0e6812334d8084d861
matsyk95 <matsyk998@gmail.com>
2016-12-12 22:09:19 +0100

    Update print.php

--- print.php
+++ print.php
@@ -12,24 +12,23 @@ header('Location: index.php');
 	<link href="style2_p3.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<div id="cent">
+	<div id="cz1">
 		<h1> Wpisane dane do formularza:</h1>
-		<div id="dane">
-		ImiÄ™: <?php echo $_SESSION['name'];?><br>
+		<div id="wypisanedane">
+		Imie™: <?php echo $_SESSION['name'];?><br>
 		Nazwisko: <?php echo $_SESSION['surname'];?><br>
 		Data urodzenia:	<?php echo $_SESSION['dataur'];?><br>
 		Pesel: <?php echo $_SESSION['pesel'];?><br>
 		Wiek: <?php echo $_SESSION['wiek'];?><br>
-		PleÄ‡: <?php echo $_SESSION['plec'];?><br>
+		Plec‡: <?php echo $_SESSION['plec'];?><br>
 		Komentarz: <?php echo $_SESSION['komentarz'];?><br>
 			
 		</div>
 	</div>
-	<div id="napisDol">
+	<div id="cz2">
 			Programowanie internetowe
-		<<!--	<a href="http://validator.w3.org/check?uri=referer">walidacjaHTML</a>
+			<a href="http://validator.w3.org/check?uri=referer">walidacjaHTML</a>
 			<a href="http://jigsaw.w3.org/css-validator/check/referer">walidacja CSS</a>
-			<a href="https://github.com/krzSobin/pi_3">kod ĹşrĂłdĹ‚owy</a> -->
 			<b id="logout"><a href="logout.php">Log Out</a></b>
 		</div>
 </body>
